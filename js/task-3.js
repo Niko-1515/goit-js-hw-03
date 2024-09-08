48ec88bda80ff0b0d67ea3d93b2c2a3dba36cb9a
Niko <nikodimstroy@gmail.com>
2024-09-08 15:58:25 +0200

    end game

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+    let numbArray = [];
+    
+    for (const element of numbers) {
+        if (element > value) {
+            numbArray.push(element);
+        }
+    }
+    return numbArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
